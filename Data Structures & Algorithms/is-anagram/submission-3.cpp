bbcb5920f4d174071d2f4ce17b66b9abcc7b503f
Pratik Bhatta <pratikbhatta07@gmail.com>
2026-08-15 20:42:49 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        vector<int> count(26);
+
+        for(int i = 0; i < s.size(); i++) {
+            count[s[i] - 'a']++;
+        }
+        for(int i = 0; i < t.size(); i++) {
+            count[t[i] - 'a']--;
+        }
+
+        for(int c : count) {
+            if(c != 0) return false;
+        }
+        return true;
+    }
+};
